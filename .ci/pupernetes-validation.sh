e4b0b066a37c3f406b112e76ee6aeb4985afe0f5
JulienBalestra <julien.balestra@gmail.com>
2018-07-03 01:06:56 +0200

    ci: improve the validation process

--- .ci/pupernetes-validation.sh
+++ .ci/pupernetes-validation.sh
@@ -3,14 +3,16 @@
 set -exo pipefail
 
 cd $(dirname $0)
-kubectl get hpa,svc,ep,ds,deploy,job,po --all-namespaces -o wide
+kubectl get no -o wide
+kubectl get svc,ep,ds,deploy,job,po --all-namespaces -o wide
 kubectl apply -f pupernetes-validation.yaml
 
 set +e
 while true
 do
+    kubectl get no
+    kubectl get svc,ep,ds,deploy,job,po --all-namespaces -o wide
     curl -fs "http://127.0.0.1:${PUPERNETES_API:-8989}/ready" -w '\n' || exit 0
-    kubectl get hpa,svc,ep,ds,deploy,job,po --all-namespaces -o wide
     kubectl get po -n validation -o json | jq -re '.items[] | select(.status.phase=="Succeeded")' && exit 0
-    sleep 3
+    sleep 1
 done
