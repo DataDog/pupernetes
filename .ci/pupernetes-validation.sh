6590807d9296e6b6e4dab83d6600faf50e6432c2
JulienBalestra <julien.balestra@datadoghq.com>
2018-07-03 11:32:58 +0200

    validation: improve the script

--- .ci/pupernetes-validation.sh
+++ .ci/pupernetes-validation.sh
@@ -5,6 +5,10 @@ set -exo pipefail
 cd $(dirname $0)
 kubectl get no -o wide
 kubectl get svc,ep,ds,deploy,job,po --all-namespaces -o wide
+
+curl -fv "http://127.0.0.1:${PUPERNETES_API:-8989}/metrics"
+curl -fv "http://127.0.0.1:${PUPERNETES_API:-8989}/ready" -w '\n'
+
 kubectl apply -f pupernetes-validation.yaml
 
 set +e
@@ -12,7 +16,16 @@ while true
 do
     kubectl get no
     kubectl get svc,ep,ds,deploy,job,po --all-namespaces -o wide
-    curl -fs "http://127.0.0.1:${PUPERNETES_API:-8989}/ready" -w '\n' || exit 0
-    kubectl get po -n validation -o json | jq -re '.items[] | select(.status.phase=="Succeeded")' && exit 0
+
+    # Job is completed and succeeded
+    kubectl get po -n validation -o json | jq -re '.items[] | select(.status.phase=="Succeeded")' && break
+
+    # Job is completed and succeeded but we didn't have time to collect its stats.phase because p8s already cleaned up
+    curl -fs "http://127.0.0.1:${PUPERNETES_API:-8989}/ready" -w '\n' || {
+        echo "pupernetes is already stopped"
+        break
+    }
     sleep 1
 done
+
+exit 0
