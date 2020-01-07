3738722da9c15b0e384a752d3d19327e99075528
vboulineau <vincent.boulineau@datadoghq.com>
2020-01-07 16:27:15 +0100

    Fix license check as broken by new version of wwhrd

--- scripts/update/license.sh
+++ scripts/update/license.sh
@@ -3,12 +3,13 @@
 set -exo pipefail
 
 export LC_ALL=C
+ROOT=$(git rev-parse --show-toplevel)
 
 cd $(dirname $0)/../..
 
-wwhrd list
+$ROOT/bin/wwhrd list
 
 echo Component,Origin,License > LICENSE-3rdparty.csv
 echo 'core,"github.com/frapposelli/wwhrd",MIT' >> LICENSE-3rdparty.csv
 
-wwhrd list |& grep "Found License" | awk '{print $5,$4}' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | sed s/" license="/,/ | sed s/package=/core,/ | sort >> LICENSE-3rdparty.csv
+$ROOT/bin/wwhrd list |& grep "Found License" | awk '{print $5,$4}' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | sed s/" license="/,/ | sed s/package=/core,/ | sort >> LICENSE-3rdparty.csv
